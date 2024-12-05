a7d6effc8e2330433dbfae2f8a73d5e1fd40cfae
mounikapujari <mounikapujari2005@gmail.com>
2024-12-05 23:19:25 +0530

    Add files via upload

---
+++ TheIdesofMarch.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class TheidesofMarch
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		if(n==15)
+		{
+		    System.out.println("yes");
+		}else{
+		    System.out.println("no");
+		    
+		}
+
+	}
+}
