f69c313eab67e1fd2066161513c7c2c032e44bed
mounikapujari <mounikapujari2005@gmail.com>
2024-12-05 23:27:42 +0530

    Add files via upload

---
+++ ChristmasGreetings.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ChristmasGreetingsS
+{
+	public static void main (String[] args) 
+	{
+	Scanner s=new Scanner(System.in);
+	int x=s.nextInt();
+	if(x==25)
+	{
+	    System.out.println("CHRISTMAS");
+	}else{
+	    System.out.println("ORDINARY");
+	}
+
+	}
+}
+    
