78207142bdd287fad1adedc2579a419bc9320118
mounikapujari <mounikapujari2005@gmail.com>
2024-12-05 23:41:04 +0530

    Add files via upload

---
+++ ACPlease.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ACPlease
+{
+	public static void main (String[] args) 
+	{
+        Scanner s=new Scanner(System.in);
+        int t=s.nextInt();
+        if(t>=1 && t<=50)
+        {
+        if(t>30)
+        {
+            System.out.println("yes");
+        }else{
+            System.out.println("no");
+        }
+        }
+
+	}
+}
