501459fd35f2b11d018a6fa79616713468c0221c
mounikapujari <mounikapujari2005@gmail.com>
2024-12-05 23:23:28 +0530

    Add files via upload

---
+++ ChefandParole.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ChefandParole
+{
+	public static void main (String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int x=s.nextInt();
+		if((x>=7)&&(x<=30)){
+		    System.out.println("yes");
+		}else{
+		    System.out.println("no");
+		}
+
+	}
+}
