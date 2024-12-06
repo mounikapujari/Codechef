024764c2f7c75a970ae74d52e5d08b7c4508a45f
mounikapujari <mounikapujari2005@gmail.com>
2024-12-06 23:03:49 +0530

    Add files via upload

---
+++ SportsSection.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int X=s.nextInt();
+		if(X==8 || X==9 || X==10){
+		    System.out.println("yes");
+		}else{
+		    System.out.println("no");
+		}
+
+	}
+}
