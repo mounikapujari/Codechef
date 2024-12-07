b31454d4dbde76ca03a9343760da6ca40d00e16a
mounikapujari <mounikapujari2005@gmail.com>
2024-12-07 21:56:19 +0530

    Add files via upload

---
+++ PuzzleHunt.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        if(N>=1 && N<=10){
+        if(N>=6 && N<=8)
+        {
+            System.out.println("yes");
+        }else{
+            System.out.println("no");
+        }
+        }
+    }
+}
