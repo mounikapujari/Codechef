464692a0ed763838ac2a477a5c32f4f40ce1424b
mounikapujari <mounikapujari2005@gmail.com>
2024-12-07 22:24:58 +0530

    Add files via upload

---
+++ AlgomaniacFinals.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Codechef{
+    public static void main (String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int X=s.nextInt();
+		if(X==17)
+		{
+		    System.out.println("YAY");
+		}else{
+		    System.out.println("NO");
+		}
+	}
+}
