4f77c47851b77994b8103afa354ea3d580f22a3f
mounikapujari <mounikapujari2005@gmail.com>
2024-12-08 20:44:10 +0530

    Add files via upload

---
+++ BatteryHealth.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    Scanner s=new Scanner(System.in);
+	    int T=s.nextInt();
+	    for(int i=0;i<T;i++){
+	        int X=s.nextInt();
+	        if(X>=80 && X<=100)
+	        {
+	            System.out.println("YES");
+	        }else{
+	            System.out.println("NO");
+	        }
+	    }
+
+	}
+}
