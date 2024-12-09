91098c9473af43ebfb6e5a4e9ff858b2d71eff77
mounikapujari <mounikapujari2005@gmail.com>
2024-12-09 22:27:06 +0530

    Add files via upload

---
+++ ValentinesContest.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int N=s.nextInt();
+		if(N>=120 && N<=123)
+		{
+		    if(N==121)
+		    {
+		        System.out.println("Likely");
+		    }
+		    else{
+		     System.out.println("unlikely");
+		    }
+		}
+	}
+}
