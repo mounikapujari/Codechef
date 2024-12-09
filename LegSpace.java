d8ce1a2c7eedd5b4f2024df97893028c2ef3e1aa
mounikapujari <mounikapujari2005@gmail.com>
2024-12-09 22:41:50 +0530

    Add files via upload

---
+++ LegSpace.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int N=s.nextInt();
+		int M=s.nextInt();
+		if(N>=1 && N<=M && M<=10 && N<M){
+		    System.out.println("YES");
+		}else{
+		    System.out.println("NO");
+		}
+		
+
+	}
+}
+
