d4db90a31868b2e271a79b1f60eba040d90d66b6
mounikapujari <mounikapujari2005@gmail.com>
2024-12-10 23:19:52 +0530

    Add files via upload

---
+++ WhoMakesP1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int A=s.nextInt();
+		int B=s.nextInt();
+		if(A>=1 && B<=10)
+		{
+		    if(A<=B){
+		        System.out.println("Tyro");
+		    }
+		        else{
+		            System.out.println("Dom");
+		        }
+		    }
+		}
+	}
