b3b72aea8a1115215616f74fbab62e49df1676d0
mounikapujari <mounikapujari2005@gmail.com>
2024-12-10 23:07:27 +0530

    Add files via upload

---
+++ ICECREAM.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int X=s.nextInt();
+		int Y=s.nextInt();
+		if((2*X)<=Y)
+		System.out.println("YES");
+		else
+		System.out.println("NO");
+		
+	}
+}
