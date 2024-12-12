2a4bb68fb06f5a7ba9c28c57eac3f225f694b51c
mounikapujari <mounikapujari2005@gmail.com>
2024-12-12 21:09:59 +0530

    Add files via upload

---
+++ VolumeControl.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int T=s.nextInt();
+		for(int i=0;i<T;i++)
+		{
+		    int X=s.nextInt();
+		    int Y=s.nextInt();
+		    if(X>Y)System.out.println(X-Y);
+		    else System.out.println(Y-X);
+		}
+
+	}
