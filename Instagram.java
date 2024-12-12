10481408036359b9e110a781b4f303d2f772dc0e
mounikapujari <mounikapujari2005@gmail.com>
2024-12-12 21:03:06 +0530

    Add files via upload

---
+++ Instagram.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s=new Scanner(System.in);
+	    int T=s.nextInt();
+	    for(int i=0;i<T;i++)
+	    {
+	        int X=s.nextInt();
+	        int Y=s.nextInt();
+	        if(X>=1 && Y<=100){
+	        if(X>Y*10)
+	        {
+	            System.out.println("YES");
+	        }else{
+	            System.out.println("NO");
+	        }
+	    }
+	    }
+	}
+}
