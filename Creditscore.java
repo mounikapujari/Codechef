48d4c9a3ee67855ece3d571cbeb984741cd725a5
mounikapujari <mounikapujari2005@gmail.com>
2024-12-14 20:11:16 +0530

    Add files via upload

---
+++ Creditscore.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int X=s.nextInt();
+		if(X>=0 && X<=1000)
+		if(X>=750) System.out.println("YES");
+		else System.out.println("NO");
+	}
+}
