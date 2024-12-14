bca64b038653b11ee40bf722ee2514d7df09fdcb
mounikapujari <mounikapujari2005@gmail.com>
2024-12-14 20:04:09 +0530

    Add files via upload

---
+++ AirQualityIndex.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int X=s.nextInt();
+		if(X>=1 && X<=150 )
+		if(X<100){
+		    System.out.println("YES");
+		}
+		else{
+		    System.out.println("NO");
+		}
+	}
+}
