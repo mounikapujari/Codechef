502faeeb97e347a671e12399c3ac216c032957bd
mounikapujari <mounikapujari2005@gmail.com>
2024-12-15 19:55:17 +0530

    Add files via upload

---
+++ IncreaseIQ.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int X=s.nextInt();
+		if(X>=100 && X<=169)
+		if(X +7>170) System.out.println("yes");
+		else System.out.println("No");
+	}
+}
