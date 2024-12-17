525b935d2f7cf575e4983d50f76a1f74e28143c0
mounikapujari <mounikapujari2005@gmail.com>
2024-12-17 21:32:01 +0530

    Add files via upload

---
+++ TheThreeTopics.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner s=new Scanner(System.in);
+        int A=s.nextInt();
+        int B=s.nextInt();
+        int C=s.nextInt();
+        int X=s.nextInt();
+        if(X==A || X==B || X==C){
+            System.out.println("YES");
+        }else
+        System.out.println("NO");
+	}
+}
