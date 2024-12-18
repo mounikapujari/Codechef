122d16e380b50f5c29e708f71e4e1a562bee40a2
mounikapujari <mounikapujari2005@gmail.com>
2024-12-18 22:07:22 +0530

    Add files via upload

---
+++ ChristmasCake.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int X=s.nextInt();
+		int Practice;
+		if(X>=1 && X<=24){
+		Practice=25-X;
+		System.out.println(Practice);
+		}
+
+	}
+}
