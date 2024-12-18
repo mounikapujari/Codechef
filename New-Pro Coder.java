a9c5bc22a79a86312d4f77e700db526868a48396
mounikapujari <mounikapujari2005@gmail.com>
2024-12-18 22:09:32 +0530

    Add files via upload

---
+++ New-Pro Coder.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int N=s.nextInt();
+		int M=s.nextInt();
+		if(M<=N){
+		  if(M>=((N+1)/2))
+		   {
+		    System.out.println("NEWBIE");
+		}else{
+		    System.out.println("PRO");
+		}
+	}
+}
+}
