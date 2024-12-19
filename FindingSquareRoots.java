91cc0ab8ac2838f034718e9d07c578341b889e7c
mounikapujari <mounikapujari2005@gmail.com>
2024-12-19 21:28:26 +0530

    Add files via upload

---
+++ FindingSquareRoots.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int T=s.nextInt();
+		for(int i=0;i<T;i++){
+		int N=s.nextInt();
+		System.out.println((int)(Math.sqrt(N)));
+
+	}
+  }
+}
