3a738d578b352ed9186cb6eda2f7575980455c80
mounikapujari <mounikapujari2005@gmail.com>
2024-12-20 21:54:01 +0530

    Add files via upload

---
+++ ImportantPagesonCodeChef.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int A=s.nextInt();
+		int B=s.nextInt();
+		if(A==0){
+		    System.out.println("https://www.codechef.com/practice");
+		}else if(A==1 && B==0){
+		    System.out.println("https://www.codechef.com/contests");
+		}else{
+		    System.out.println("https://discuss.codechef.com");
+		}
+
+	}
+}
