76735ba35f1e4be5d88511e76cf5bdc2ff6a20f7
mounikapujari <mounikapujari2005@gmail.com>
2024-12-21 22:06:14 +0530

    Add files via upload

---
+++ SumORDifference.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   Scanner s=new Scanner(System.in);
+	   int N1=s.nextInt();
+	   int N2=s.nextInt();
+	   if(N1>N2){
+	       System.out.println(N1-N2);
+	   }else System.out.println(N1+N2);
+
+	}
+}
