a9ff61517d25ee01a11ce31033cc598f0c2d6f74
mounikapujari <mounikapujari2005@gmail.com>
2024-12-22 20:07:43 +0530

    Add files via upload

---
+++ HelpingChef.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s=new Scanner(System.in);
+	    int T=s.nextInt();
+	    for(int i=0;i<T;i++){
+	        int N=s.nextInt();
+	        if(N<10) System.out.println("Thanks for helping Chef!");
+	        else System.out.println("-1");
+	    }
+
+	}
+}
