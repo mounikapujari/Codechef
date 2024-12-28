b646fbe629977247a742a0e9c972da3c59852740
mounikapujari <mounikapujari2005@gmail.com>
2024-12-28 20:12:42 +0530

    Add files via upload

---
+++ SummerTime.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int X=s.nextInt();
+		if(X>35) System.out.println("YES");
+		else System.out.println("NO");
+
+	}
+}
