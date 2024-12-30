3142e4913933f8294cdc2fc71f98bd9d6a8c9776
mounikapujari <mounikapujari2005@gmail.com>
2024-12-30 19:19:33 +0530

    Add files via upload

---
+++ IsitaVOWELorCONSONANT.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		char C=s.next().charAt(0);
+		if(C=='A'||C=='E'||C=='I'||C=='O'||C=='U'){
+		    System.out.println("vowel");
+		}else System.out.println("Consonant");
+
+	}
+}
