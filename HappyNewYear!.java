ebe6ee817c05601860ca2e9bb9215bfd180b3c5d
mounikapujari <mounikapujari2005@gmail.com>
2025-01-03 20:26:31 +0530

    Add files via upload

---
+++ HappyNewYear!.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int X=s.nextInt();
+		if(X>=0 && X<=23)
+		System.out.println(24-X);
+	}
+}
+
