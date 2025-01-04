8a88b7f3646a3ce571211f84547ad19125e9e668
mounikapujari <mounikapujari2005@gmail.com>
2025-01-04 18:38:19 +0530

    Add files via upload

---
+++ DiwaliDiscount.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s=new Scanner(System.in);
+	    int A=s.nextInt();
+	    int B=s.nextInt();
+	    if(B<A){
+	        System.out.println(A-B);
+	    }else System.out.println("0");
+	    
+
+	}
+}
