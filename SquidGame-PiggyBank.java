5fc7183179ff4b36a8a06963fd551acd7ce52bd2
mounikapujari <mounikapujari2005@gmail.com>
2025-02-28 10:51:09 +0530

    Add files via upload

---
+++ SquidGame-PiggyBank.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int N=s.nextInt();
+		int K=s.nextInt();
+		System.out.println((N-K)*10000);
+	}
+}
