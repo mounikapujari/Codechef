1da8f7cb2b72e49310ac3681be6b15ccc2306123
mounikapujari <mounikapujari2005@gmail.com>
2025-02-28 10:37:01 +0530

    Add files via upload

--- HappyNewYear!.java
+++ HappyNewYear!.java
@@ -1,14 +1,15 @@
-import java.lang.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner s=new Scanner(System.in);
-		int X=s.nextInt();
-		if(X>=0 && X<=23)
-		System.out.println(24-X);
+			Scanner s=new Scanner(System.in);
+			int x=s.nextInt();
+			if(x>=0 && x<=24){
+			    System.out.println(24-x);
+			}
 	}
 }
-
