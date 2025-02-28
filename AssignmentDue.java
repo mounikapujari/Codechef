c22248c265e445d02c1d1c25c866bfd0ed5c82e3
mounikapujari <mounikapujari2005@gmail.com>
2025-02-28 10:42:57 +0530

    Add files via upload

---
+++ AssignmentDue.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int x=s.nextInt();
+		int y=s.nextInt();
+		if(x<=y){
+		    System.out.println("YES");
+		}else{
+		    System.out.println("NO");
+		}
+
+	}
+}
