b1804c4d023da831628526aeba3893662eef3e20
mounikapujari <mounikapujari2005@gmail.com>
2025-02-28 10:58:51 +0530

    Add files via upload

---
+++ TooMuchHomework!.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int x=s.nextInt();
+		int y=s.nextInt();
+		int z;
+		z=x+y*10;
+		if(z>=100){
+		    System.out.println("Yes");
+		}else {
+		    System.out.println("No");
+		}
+
+	}
+}
