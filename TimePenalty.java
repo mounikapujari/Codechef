a9bd12bb2aeaefd4582e25baa8cbc33af8a24785
mounikapujari <mounikapujari2005@gmail.com>
2025-02-28 10:32:02 +0530

    Add files via upload

---
+++ TimePenalty.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int x=s.nextInt();
+		int y=s.nextInt();
+		System.out.println(x+(y*10));
+
+	}
+}
